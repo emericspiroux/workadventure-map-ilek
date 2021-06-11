750608efd646ef328459a9586b1bba0ebe6e22ad
Emeric <emeric@ilek.fr>
2021-06-11 15:13:55 +0200

    New world edit

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,4 @@
 // The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
-WA.sendChatMessage('Hello world', 'Mr Robot');
-
-WA.onEnterZone('myZone', () => {
-  WA.sendChatMessage('You enter inside my zone!', 'Mr Robot');
-});
+WA.sendChatMessage('Bienvenu chez Ilek', 'Houston');
