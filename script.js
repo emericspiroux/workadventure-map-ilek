7f1b8525d730ef81962c44037008cf9d34e70728
emericspiroux <espiroux@student.42.fr>
2021-06-11 09:30:24 +0000

    Deploying to gh-pages from master @ 1bb0651dff6c3aef7dab0eab8fc8d16c6165a0dc 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://ilek.fr")})();
+(()=>{"use strict";console.log("Script started successfully"),WA.sendChatMessage("Hello world","Mr Robot")})();
 //# sourceMappingURL=script.js.map
