992a2b7f0d6a9f7630a7421185c1317c325b4d4d
emericspiroux <espiroux@student.42.fr>
2021-06-11 13:43:52 +0000

    Deploying to gh-pages from master @ 9946b89b8df75eb6d8017bdc216f7e38d9f7dde2 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.sendChatMessage("Bienvenue chez Ilek !","Houston"),WA.sendChatMessage("Ceci est le test 12","Houston")})();
+(()=>{"use strict";console.log("Script started successfully"),WA.sendChatMessage("Bienvenue chez Ilek !","Houston"),WA.sendChatMessage("Ceci est le test 13","Houston")})();
 //# sourceMappingURL=script.js.map
